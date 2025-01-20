352e227aec9477eb85d57dbb56deb05fb3a2c00c
Parlows <pablo.regodon@hotmail.com>
2025-01-20 15:35:09 +0100

    changed names typo

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@ docker stop postgres
 docker rm postgres
 docker image rm pgsurv
 docker build . -t pgsurv
-docker run -it --name pgext -e PGDATA=/data/ -e POSTGRES_HOST_AUTH_METHOD=trust -v /media/data/pg/:/data -p 5432:5432 postgres
+docker run -it --name postgres -e PGDATA=/data/ -e POSTGRES_HOST_AUTH_METHOD=trust -v /media/data/pg/:/data -p 5432:5432 pgsurv
